efc9e04a41a17e846b4818ad2014dde20dc841ca
kymwithay <hillkymberlee@gmail.com>
2017-11-16 14:53:49 -0500

    adding local projects and changes to the remote respository

--- search_two_strings/searchtwostrings/platforms/android/assets/www/js/app.js
+++ search_two_strings/searchtwostrings/platforms/android/assets/www/js/app.js
@@ -8,20 +8,30 @@
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
+    $('#search-first-name', '#search-last-name').on('keyup', findByName);
     $('.help-btn').on('click', function() {
         alert("Employee Directory v3.4");
     });
 
+
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
+
+        var checkFirstname = $('#search-first-name').val().trim():
+        var checkLastname = $('#search-last-name').val().trim();
+
+        service.findByName($('#search-first-name, #search-last-name').val()).done(function (employees) {
             var l = employees.length;
             var e;
             $('.employee-list').empty();
+            if (checkFirstname.length >= 2 || checkLastname.length >= 2){
             for (var i = 0; i < l; i++) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+            else{
+             $('.employee-list').empty();
+            }
             }
         });
     }
