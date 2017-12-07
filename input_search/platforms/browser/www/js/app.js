df209eff64eae1cd7327e65def2f31c61db21b5c
kymwithay <hillkymberlee@gmail.com>
2017-12-07 00:01:44 -0500

    added a div id called app and replaced the body tag

--- input_search/platforms/browser/www/js/app.js
+++ input_search/platforms/browser/www/js/app.js
@@ -4,16 +4,17 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
+        renderHomeView();
         console.log("Service initialized");
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('#search-first-name, #search-last-name').on('keyup', findByName); //changed at 11:48pm
+//    $('#search-first-name, #search-last-name').on('keyup', findByName); //changed at 11:48pm
     //$('.search-last-key').on('keyup', findByName); //changed from search-key at 11:34pm
 
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+//    $('.help-btn').on('click', function() {
+//        alert("Employee Directory v3.4");
+//    });
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
@@ -38,5 +39,15 @@
             }
         });
     }
+
+    function renderHomeView() {
+        var html =
+         '<h1>Directory</h1>' +
+         '<input id= "search-first-name" type="search" placeholder="Enter First Name"/>' +
+         '<input id= "search-last-name" type="search" placeholder="Enter Last Name"/>' +
+         '<ul class= "employee-list"></ul>';
+          $('body').html(html);
+          $('#search-first-name, #search-last-name').on('keyup', findByName);
+          }
 
 }());
